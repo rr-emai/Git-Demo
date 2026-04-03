a23f5567257945333a311580c5816b8d9b34275d
Rohan <rohan@easemyai.com>
2026-04-03 17:32:28 +0530

    added-featureB-in-aboutpage

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
 import { type JSX } from "react";
 
 const About = (): JSX.Element => {
-  return <h1 style={{ color: "red" }}>About Routes</h1>;
+  return (
+    <>
+      <h1 style={{ color: "red" }}>About Routes</h1>
+      <h2>This is from feature B</h2>
+    </>
+  );
 };
 
 export default About;
