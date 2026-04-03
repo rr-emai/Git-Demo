3bb4512d148d131ff10ad09dd40ac245c30faa5d
Rohan <rohan@easemyai.com>
2026-04-03 17:37:39 +0530

    fixed:bracket

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,12 +4,11 @@ const About = (): JSX.Element => {
   return (
     <>
       (
-    <>
-      <h1 style={{ color: "red" }}>About Routes</h1>
-      <h2>This is from Feature A</h2>
-    </>
-  )
-      <h2>This is from feature B</h2>
+      <>
+        <h1 style={{ color: "red" }}>About Routes</h1>
+        <h2>This is from Feature A</h2>
+      </>
+      )
     </>
   );
 };
