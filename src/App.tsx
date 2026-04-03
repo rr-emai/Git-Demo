5d09bb4cd49144f00a9499fd41f8765976a16018
Rohan <rohan@easemyai.com>
2026-04-03 17:15:30 +0530

    added-about-page and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { type JSX } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App(): JSX.Element {
   return (
     <>
       <Routes>
         <Route path="/home" element={<h2>Home Routes</h2>} />
-        {/* <Route /> */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+import { type JSX } from "react";
+
+const About = (): JSX.Element => {
+  return <h1 style={{ color: "red" }}>About Routes</h1>;
+};
+
+export default About;
