5bf6662e3679779e7839d64f72b015bfe7664b01
Rohan <rohan@easemyai.com>
2026-04-03 18:37:10 +0530

    feat: another feature added in practice

---
+++ src/pages/Practice.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Practice = () => {
+  return (
+    <div>
+      <h2>Practice</h2>
+      <p>Another Feature Added In Practice</p>
+      <p>Another Feature Added In Practice</p>
+    </div>
+  );
+};
+
+export default Practice;
